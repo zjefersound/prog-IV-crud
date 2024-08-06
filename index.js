979bff5c3c1a99d5944dcd1e8d7f736623436161
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-05 22:45:11 -0300

    Fix: Edit card + create card bugs

--- index.js
+++ index.js
@@ -34,7 +34,13 @@ renderCards();
 
 const form = document.getElementById("cardForm");
 form.onsubmit = handleSubmit;
-
+function clearInputs() {
+  document.getElementById("cardId").value = "";
+  document.getElementById("cardNumber").value = "";
+  document.getElementById("expirationDate").value = "";
+  document.getElementById("securityCode").value = "";
+  document.getElementById("form-title").innerText = "Adicionar cartão";
+}
 function handleSubmit(e) {
   e.preventDefault();
   const cardId = document.getElementById("cardId");
@@ -57,12 +63,11 @@ function handleSubmit(e) {
     });
   }
   renderCards();
-  cardNumber.value = "";
-  expirationDate.value = "";
-  securityCode.value = "";
+  clearInputs();
 }
 
 function editCard(cardId) {
+  document.getElementById("form-title").innerText = "Editar cartão";
   const card = cardStorage.getById(cardId);
   document.getElementById("cardId").value = card.id;
   document.getElementById("cardNumber").value = card.cardNumber;
@@ -73,6 +78,7 @@ function editCard(cardId) {
 function deleteCard(cardId) {
   cardStorage.delete(cardId);
   renderCards();
+  clearInputs();
 }
 
 function renderCards() {
@@ -93,7 +99,7 @@ function renderCards() {
         Editar
       </button>
       <button class="delete-btn" onclick="deleteCard('${c.id}')">
-        Deletar
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" id="delete"><path d="M24.2,12.193,23.8,24.3a3.988,3.988,0,0,1-4,3.857H12.2a3.988,3.988,0,0,1-4-3.853L7.8,12.193a1,1,0,0,1,2-.066l.4,12.11a2,2,0,0,0,2,1.923h7.6a2,2,0,0,0,2-1.927l.4-12.106a1,1,0,0,1,2,.066Zm1.323-4.029a1,1,0,0,1-1,1H7.478a1,1,0,0,1,0-2h3.1a1.276,1.276,0,0,0,1.273-1.148,2.991,2.991,0,0,1,2.984-2.694h2.33a2.991,2.991,0,0,1,2.984,2.694,1.276,1.276,0,0,0,1.273,1.148h3.1A1,1,0,0,1,25.522,8.164Zm-11.936-1h4.828a3.3,3.3,0,0,1-.255-.944,1,1,0,0,0-.994-.9h-2.33a1,1,0,0,0-.994.9A3.3,3.3,0,0,1,13.586,7.164Zm1.007,15.151V13.8a1,1,0,0,0-2,0v8.519a1,1,0,0,0,2,0Zm4.814,0V13.8a1,1,0,0,0-2,0v8.519a1,1,0,0,0,2,0Z"></path></svg>
       </button>
     `;
     listEl.appendChild(item);
